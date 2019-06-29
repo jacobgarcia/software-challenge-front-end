69379eb324c20ac9ab35ea98894809a8db09aa57
Jacob García <rock2494@hotmail.com>
2019-06-28 20:03:31 -0500

    Refactor App

---
+++ src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ScanContainer from 'ScanContainer';
+
+import './index.css';
+
+const Challenge = () => (
+  <div className='app'>
+    <header className='app-header'>Software Challenge</header>
+    <ScanContainer />
+  </div>
+);
+
+export default Challenge;
